7a02c4e342654a50b4a7f2de238382d59f2161f4
roygb1v <cjhroy@outlook.com>
2019-01-25 03:26:42 +0800

    first commit

---
+++ test.rb
@@ -0,0 +1,8 @@
+file = File.read('hello.txt')
+
+arr = [1,2,3]
+arr.each do |x|
+    p x
+end
+
+
---
+++ user.rb
@@ -0,0 +1,12 @@
+loop do
+	puts "Do you wish to do that again ?"
+	answer = gets.chomp
+	if answer == 'STOP'
+		break
+	end
+end
+
+
+
+
+
